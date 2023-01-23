f6cb5bbce5ef09205f75f05976be7e9c3d1cce14
Kim Fehrs <kim.fehrs@rdx.works>
2023-01-23 14:09:57 +0100

    introduce config command
    this is basically a refactor of the old install command. It extracts all commands that generate variables from arguments or fetching from github.
    The goal is to have a shareable configfile to setup multiple nodes in a similar fashion.

--- node-runner-cli/tests/test_systemd.py
+++ node-runner-cli/tests/test_systemd.py
@@ -1,19 +1,21 @@
 import unittest
 import os
 import subprocess
+from unittest.mock import patch
 
 from setup import SystemD
 from utils.Prompts import Prompts
 from utils.PromptFeeder import PromptFeeder
 from utils.utils import run_shell_command
-
+from radixnode import main
 
 class SystemdUnitTests(unittest.TestCase):
-    @unittest.skip("Needs enough balance")
+
+    @unittest.skip("Tests with PROMPT_FEEDS can only be run individually")
     def test_systemd_install_continue_prompt_feed(self):
         os.environ['PROMPT_FEEDS'] = "test-prompts/individual-prompts/systemd_install_continue.yml"
-        PromptFeeder.instance()
-        Prompts.confirm_systemd_install("dummy1", "dummy2", "dummy3", "dummy4")
+        PromptFeeder.instance().load_prompt_feeds()
+        SystemD.confirm_config("dummy1", "dummy2", "dummy3", "dummy4")
 
     def test_systemd_install_requires_radixdlt_user(self):
         pass
@@ -21,43 +23,37 @@ class SystemdUnitTests(unittest.TestCase):
     def test_systemd_install_generates_keyfile(self):
         pass
 
-    @unittest.skip("Needs enough balance")
     def test_systemd_install_sets_environments_by_creating_file(self):
         SystemD.set_environment_variables("dummypw", "/tmp")
         self.assertTrue(os.path.isfile("/tmp/environment"))
 
-    @unittest.skip("Needs enough balance")
-    def test_systemd_creates_default_config(self):
+    @unittest.skip("Tests with PROMPT_FEEDS can only be run individually")
+    def test_systemd_creates_default_config_without_asking(self):
         os.environ['PROMPT_FEEDS'] = "test-prompts/individual-prompts/systemd_install_default_config.yml"
-        PromptFeeder.instance()
+        PromptFeeder.instance().load_prompt_feeds()
         SystemD.setup_default_config("radix:12345dummynode", "1.1.1.1", "/tmp", "???", "false")
         self.assertTrue(os.path.isfile("/tmp/default.config"))
         # ToDo: Test Appending of override lines
 
-    @unittest.skip("Needs enough balance")
-    def test_systemd_creates_service_file(self):
+    @unittest.skip("Tests with PROMPT_FEEDS can only be run individually")
+    def test_systemd_creates_service_file_without_asking(self):
         os.environ['PROMPT_FEEDS'] = "test-prompts/individual-prompts/systemd_install_default_config.yml"
+        PromptFeeder.instance().load_prompt_feeds()
         PromptFeeder.instance()
-        SystemD.setup_service_file("someversion", "/tmp", "/tmp/secrets")
-        self.assertTrue(os.path.isfile("/etc/systemd/system/radixdlt-node.service"))
+        SystemD.setup_service_file("someversion", "/tmp", "/tmp/secrets", "/tmp/servicefile")
+        self.assertTrue(os.path.isfile("/tmp/servicefile"))
 
-    @unittest.skip("can not get it to work")
-    def test_systemd_depency_create_service_user_password_optional_input(self):
-        # passwd_input= "test\ntest\ntest\n"
-        # subprocess.run("cat", input=passwd_input.encode(), shell=False)
-        # SystemD.create_service_user_password(oldpassword="mypassword",username="kim.fehrs")
+    def test_systemd_config_can_run_without_prompt(self):
+        with patch("sys.argv",
+                   ["main", "systemd", "config",
+                    "-a", "true",
+                    "-t", "somenode",
+                    "-i", "123.123.123.123",
+                    "-kp", "password",
+                    "-n", "S",
+                    "-dd", "/tmp/data"]):
+            main()
 
-    def test_systemd_downloads_binaries(self):
-        pass
-
-    def test_systemd_creates_nginx_config(self):
-        pass
-
-    def test_systemd_starts_nginx_service(self):
-        pass
-
-    def test_systemd_starts_core_service(self):
-        pass
 
 
 def suite():
