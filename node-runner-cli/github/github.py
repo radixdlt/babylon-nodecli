c46b67816135498d0638a55c49112bebf25e6615
Kim Fehrs <kim.fehrs@rdx.works>
2023-01-24 13:48:15 +0100

    add option to fetch latest release with GITHUB_TOKEN

--- node-runner-cli/github/github.py
+++ node-runner-cli/github/github.py
@@ -1,4 +1,5 @@
 import json
+import os
 import sys
 
 import requests
@@ -9,9 +10,12 @@ def latest_release(repo_name="radixdlt/radixdlt"):
     req = requests.Request('GET',
                            f'https://api.github.com/repos/{repo_name}/releases/latest')
 
+    token = os.getenv('GITHUB_TOKEN')
     prepared = req.prepare()
     prepared.headers['Content-Type'] = 'application/json'
     prepared.headers['user-agent'] = 'radixnode-cli'
+    if token is not None:
+        prepared.headers['Authorization'] = token
     resp = Helpers.send_request(prepared, print_response=False)
     if not resp.ok:
         print("Failed to get latest release from github. The response was:")
