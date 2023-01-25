9b86199c6685c4980afb137914324b6f5184906c
Kim Fehrs <kim.fehrs@rdx.works>
2023-01-25 14:56:03 +0100

    fix sys.exit command

--- node-runner-cli/commands/monitoring.py
+++ node-runner-cli/commands/monitoring.py
@@ -1,3 +1,4 @@
+import sys
 from argparse import ArgumentParser
 from os.path import exists
 
@@ -25,7 +26,7 @@ def read_monitoring_config(args):
     yaml.add_representer(type(None), Helpers.represent_none)
     if not exists(args.monitoringconfigfile):
         print("There is no monitoring config file. It seems like monitoring was not set up. Nothing to do here")
-        exit(0)
+        sys.exit(0)
     with open(args.monitoringconfigfile, 'r') as file:
         all_config = yaml.safe_load(file)
     return all_config
