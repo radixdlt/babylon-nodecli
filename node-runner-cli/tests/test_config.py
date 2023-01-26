b96def79322f5d331a107032a4c14cfe866652c6
Kim Fehrs <kim.fehrs@rdx.works>
2023-01-26 13:07:42 +0100

    add tests for extracting network_id

--- node-runner-cli/tests/test_config.py
+++ node-runner-cli/tests/test_config.py
@@ -6,7 +6,7 @@ import yaml
 from yaml import UnsafeLoader
 
 from config.DockerConfig import DockerConfig
-from config.SystemDConfig import SystemDSettings
+from config.SystemDConfig import SystemDSettings, extract_network_id_from_arg
 from config.Nginx import SystemdNginxConfig
 from config.KeyDetails import KeyDetails
 from radixnode import main
@@ -116,6 +116,16 @@ gateway_settings:
     host: host.docker.internal:5432
 """
         self.assertEqual(config_as_yaml, fixture)
+
+    def test_network_id_can_be_parsed(self):
+        self.assertEqual(extract_network_id_from_arg("1"), 1)
+        self.assertEqual(extract_network_id_from_arg("m"), 1)
+        self.assertEqual(extract_network_id_from_arg("M"), 1)
+        self.assertEqual(extract_network_id_from_arg("mainnet"), 1)
+        self.assertEqual(extract_network_id_from_arg("2"), 2)
+        self.assertEqual(extract_network_id_from_arg("s"), 2)
+        self.assertEqual(extract_network_id_from_arg("S"), 2)
+        self.assertEqual(extract_network_id_from_arg("stokenet"), 2)
 
 
 
