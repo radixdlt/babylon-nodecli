8171832ccba61e243b2ca9ecbf6905ccd477e25b
Kim Fehrs <kim.fehrs@rdx.works>
2023-01-30 13:56:53 +0100

    fix system --manual

--- node-runner-cli/tests/test_systemd.py
+++ node-runner-cli/tests/test_systemd.py
@@ -78,9 +78,15 @@ class SystemdUnitTests(unittest.TestCase):
 
     def test_systemd_dependencies(self):
         with patch("sys.argv",
-                   ["main", "systemd", "config","-m", "CORE", "-i", "18.133.170.30", "-t",
+                   ["main", "systemd", "config", "-m", "CORE", "-i", "18.133.170.30", "-t",
                     "radix://tn1q28eygvxshszxk48jhjxdmyne06m3x6hfyvxg7a45qt8cksffx6z7uu6392@15.236.228.96",
                     "-n", "2", "-k", "radix", "-d", "/tmp", "-dd", "/tmp", "-a"]):
+            main()
+
+    @unittest.skip("For verification only")
+    def test_systemd_install_manual(self):
+        with patch("sys.argv",
+                   ["main", "systemd", "install", "-a", "-m", "-f", "/tmp/node-config/test-config.yaml"]):
             main()
 
 
