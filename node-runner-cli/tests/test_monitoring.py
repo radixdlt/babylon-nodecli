4fc1172e1ea7896fcaf7a4cbacbc702ceb83bcb0
Kim Fehrs <kim.fehrs@rdx.works>
2023-02-21 07:45:30 -0500

    refacotor Testsuite name

--- node-runner-cli/tests/test_monitoring.py
+++ node-runner-cli/tests/test_monitoring.py
@@ -2,12 +2,14 @@ import os.path
 import unittest
 from monitoring import Monitoring
 
-class MyTestCase(unittest.TestCase):
+
+class MonitoringTests(unittest.TestCase):
 
     def test_template(self):
         Monitoring.template_dashboards(["dashboard.yml", "babylon-node-dashboard.json", "babylon-jvm-dashboard.json",
                                         "network-gateway-dashboard.json"], "/tmp")
         self.assertTrue(os.path.exists("/tmp/grafana/provisioning/dashboards/network-gateway-dashboard.json"))
+
 
 if __name__ == '__main__':
     unittest.main()
