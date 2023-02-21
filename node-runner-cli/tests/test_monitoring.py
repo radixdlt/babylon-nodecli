c4fffdb0a575d79ed3567208052f53cc733fb986
Kim Fehrs <kim.fehrs@rdx.works>
2023-02-21 07:55:56 -0500

    add a test about missing templates

--- node-runner-cli/tests/test_monitoring.py
+++ node-runner-cli/tests/test_monitoring.py
@@ -1,5 +1,8 @@
 import os.path
 import unittest
+from io import StringIO
+from unittest import mock
+from jinja2.exceptions import TemplateNotFound
 from monitoring import Monitoring
 
 
@@ -12,6 +15,13 @@ class MonitoringTests(unittest.TestCase):
         self.assertTrue(os.path.exists("/tmp/grafana/provisioning/dashboards/dashboard.yml"))
         self.assertTrue(os.path.exists("/tmp/grafana/provisioning/dashboards/babylon-jvm-dashboard.json"))
         self.assertTrue(os.path.exists("/tmp/grafana/provisioning/dashboards/network-gateway-dashboard.json"))
+
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    def test_template_failure(self, mock_stdout):
+        with self.assertRaises(TemplateNotFound) as cm:
+            Monitoring.template_dashboards(["this-template-does-not-exist"], "/tmp")
+            self.assertEqual(mock_stdout.getvalue(), "jinja2.exceptions.TemplateNotFound: this-template-does-not-exist.j2")
+            self.assertEqual(cm.exception.code, 1)
 
 
 if __name__ == '__main__':
