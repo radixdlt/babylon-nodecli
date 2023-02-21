506bf55bf47849e0cea6f6a326c661c90f4ebc2e
Kim Fehrs <kim.fehrs@rdx.works>
2023-02-21 07:46:36 -0500

    update test to verify existence of all templated files

--- node-runner-cli/tests/test_monitoring.py
+++ node-runner-cli/tests/test_monitoring.py
@@ -9,6 +9,9 @@ class MonitoringTests(unittest.TestCase):
         Monitoring.template_dashboards(["dashboard.yml", "babylon-node-dashboard.json", "babylon-jvm-dashboard.json",
                                         "network-gateway-dashboard.json"], "/tmp")
         self.assertTrue(os.path.exists("/tmp/grafana/provisioning/dashboards/network-gateway-dashboard.json"))
+        self.assertTrue(os.path.exists("/tmp/grafana/provisioning/dashboards/dashboard.yml"))
+        self.assertTrue(os.path.exists("/tmp/grafana/provisioning/dashboards/babylon-jvm-dashboard.json"))
+        self.assertTrue(os.path.exists("/tmp/grafana/provisioning/dashboards/network-gateway-dashboard.json"))
 
 
 if __name__ == '__main__':
