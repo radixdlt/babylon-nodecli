a9fd4e148ed93f6404c0c9f2fd359a085b7d78f8
Kim Fehrs <kim.fehrs@rdx.works>
2023-07-26 12:53:26 +0200

    fix unit test bugs

--- node-runner-cli/utils/Prompts.py
+++ node-runner-cli/utils/Prompts.py
@@ -201,7 +201,7 @@ class Prompts:
     @staticmethod
     def ask_trusted_node() -> str:
         Helpers.section_headline("Trusted node settings")
-        # default_trusted_nodes = "radix://node_tdx_d_1qwq2nfe6vxqwe3mqmfm9l2xl97as7lkwndval63cymvc3qszn8nqx6g2s3m@3.109.161.178"
+        default_trusted_nodes = "radix://node_tdx_d_1qwq2nfe6vxqwe3mqmfm9l2xl97as7lkwndval63cymvc3qszn8nqx6g2s3m@3.109.161.178"
         value = Helpers.input_guestion(f"Fullnode requires another node to connect to network. "
                                        # ToDo: Add after main release of babylon
                                        # "\nTo connect to MAINNET or STOKENET details on these node can be found here "
