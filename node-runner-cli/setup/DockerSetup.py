85f2eae8b4d8fc153e5edac377b71b9aff8adf85
shambupujar <shambu.pujar@rdx.works>
2023-09-05 23:54:42 +0100

    Add condition for chown on genesis

--- node-runner-cli/setup/DockerSetup.py
+++ node-runner-cli/setup/DockerSetup.py
@@ -322,7 +322,7 @@ class DockerSetup(BaseSetup):
         username = getpass.getuser()
         run_shell_command(['sudo', 'chown', f'{username}:{username}',
                            f'{docker_config.core_node.keydetails.keyfile_path}/{docker_config.core_node.keydetails.keyfile_name}'])
-        if docker_config.common_config.genesis_bin_data_file:
+        if hasattr(docker_config.common_config, "genesis_bin_data_file"):
             run_shell_command(['sudo', 'chown', f'{username}:{username}',
                            f'{docker_config.common_config.genesis_bin_data_file}'])
         run_shell_command(['sudo', 'chown', '-R', f'{username}:{username}',
