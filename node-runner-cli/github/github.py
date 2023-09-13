112362d7d40753f2ea06dec1cf6e347ad4196e1b
Santiago Baldassin <santiago.baldassin@rdx.works>
2023-09-13 10:02:52 -0300

    improving the way we fetch releases from env vars

--- node-runner-cli/github/github.py
+++ node-runner-cli/github/github.py
@@ -1,35 +1,43 @@
 import json
-import os
+
 import sys
+from os import getenv
 
 import requests
-
-from config.EnvVars import RADIXDLT_APP_VERSION_OVERRIDE, RADIXDLT_NGINX_VERSION_OVERRIDE, RADIXDLT_CLI_VERSION_OVERRIDE, \
+from config.EnvVars import (
+    RADIXDLT_APP_VERSION_OVERRIDE,
+    RADIXDLT_NGINX_VERSION_OVERRIDE,
+    RADIXDLT_CLI_VERSION_OVERRIDE,
     RADIXDLT_GATEWAY_VERSION_OVERRIDE
+)
 from utils.utils import Helpers
+
+REPOS_ENV_VARS = {
+    "radixdlt/babylon-node": RADIXDLT_APP_VERSION_OVERRIDE,
+    "radixdlt/radixdlt-nginx": RADIXDLT_NGINX_VERSION_OVERRIDE,
+    "radixdlt/babylon-nginx": RADIXDLT_NGINX_VERSION_OVERRIDE,
+    "radixdlt/node-runner": RADIXDLT_CLI_VERSION_OVERRIDE,
+    "radixdlt/babylon-nodecli": RADIXDLT_CLI_VERSION_OVERRIDE,
+    "radixdlt/babylon-gateway": RADIXDLT_GATEWAY_VERSION_OVERRIDE,
+    "radixdlt/radixdlt-network-gateway": RADIXDLT_GATEWAY_VERSION_OVERRIDE
+}
+
+
+def get_version_override(repo_name: str) -> str:
+    return REPOS_ENV_VARS.get(repo_name, None)
 
 
 def latest_release(repo_name="radixdlt/babylon-node") -> str:
-    if repo_name == "radixdlt/babylon-node":
-        if os.environ.get(RADIXDLT_APP_VERSION_OVERRIDE) is not None:
-            return os.environ.get(RADIXDLT_APP_VERSION_OVERRIDE)
-
-    if repo_name == "radixdlt/radixdlt-nginx" or repo_name == "radixdlt/babylon-nginx":
-        if os.environ.get(RADIXDLT_NGINX_VERSION_OVERRIDE) is not None:
-            return os.environ.get(RADIXDLT_NGINX_VERSION_OVERRIDE)
-
-    if repo_name == "radixdlt/node-runner" or repo_name == "radixdlt/babylon-nodecli":
-        if os.environ.get(RADIXDLT_CLI_VERSION_OVERRIDE) is not None:
-            return os.environ.get(RADIXDLT_CLI_VERSION_OVERRIDE)
-
-    if repo_name == "radixdlt/babylon-gateway" or repo_name == "radixdlt/radixdlt-network-gateway":
-        if os.environ.get(RADIXDLT_GATEWAY_VERSION_OVERRIDE):
-            return os.environ.get(RADIXDLT_GATEWAY_VERSION_OVERRIDE)
+    version_override_env_var = get_version_override(repo_name)
+    if version_override_env_var:
+        version_override = getenv(version_override_env_var, None)
+        if version_override is not None:
+            return version_override
 
     req = requests.Request('GET',
                            f'https://api.github.com/repos/{repo_name}/releases/latest')
 
-    token = os.getenv('GITHUB_TOKEN')
+    token = getenv('GITHUB_TOKEN')
     prepared = req.prepare()
     prepared.headers['Content-Type'] = 'application/json'
     prepared.headers['user-agent'] = 'babylonnode-cli'
