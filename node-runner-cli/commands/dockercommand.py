af9ed2c5d5f675e28f673b2bde3baf2aac6210ea
Santiago Baldassin <santiago.baldassin@rdx.works>
2023-09-14 09:34:10 -0300

    ask for postgres if and only if gateway is part of the config

--- node-runner-cli/commands/dockercommand.py
+++ node-runner-cli/commands/dockercommand.py
@@ -139,7 +139,8 @@ def install(args):
     DockerSetup.confirm_config_changes(argument_object, docker_config, docker_config_updated_versions)
 
     ########## Install dependent services
-    DockerSetup.conditionally_start_local_postgres(docker_config_updated_versions)
+    if docker_config.gateway is not None:
+        DockerSetup.conditionally_start_local_postgres(docker_config_updated_versions)
 
     if docker_config.core_node is not None:
         DockerSetup.chown_files(docker_config)
