cc2a04d6b7db44be4f24cb0cfa4a5a94d526d1f0
Santiago Baldassin <santiago.baldassin@rdx.works>
2023-10-02 18:20:12 -0300

    removing extra logging

--- node-runner-cli/ret/accounts.py
+++ node-runner-cli/ret/accounts.py
@@ -12,12 +12,8 @@ from radix_engine_toolkit import (
 
 
 def derive_address(keystore: str, password: str, network_id: int) -> Address:
-    print("PEPE")
-    print(keystore)
     keystore = load_pkcs12(open(keystore,"rb").read(), password)
-    print(keystore)
     cryptography_key = keystore.get_privatekey().to_cryptography_key()
-    print(cryptography_key)
     private_key_bytes = cryptography_key.private_bytes(
         encoding=Encoding.DER,
         format=PrivateFormat.PKCS8,
