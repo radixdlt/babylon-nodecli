838e42093e89238ab22c5e2387d0be38c7ce94b7
Albert Perez Toro <albert.toro@rdx.works>
2024-10-22 09:54:41 +0200

    Formatting BaseSetup.py

--- node-runner-cli/setup/BaseSetup.py
+++ node-runner-cli/setup/BaseSetup.py
@@ -197,15 +197,15 @@ class BaseSetup:
             f"\nRadix node stores all the ledger data on a folder."
             f"Downloading latest snapshot of the ledger will allow to sync faster when starting the node"
             f"if the ledger folder is empty."
-            f'\n{bcolors.WARNING}Press Enter to accept default or choose to download latest snapshot of the ledger [true/false]: ',
+            f"\n{bcolors.WARNING}Press Enter to accept default or choose to download latest snapshot of the ledger [true/false]: ",
             QuestionKeys.input_download_snapshot_ledger,
         )
         if download_community_snapshot_string == "":
             download_community_snapshot = True
         elif download_community_snapshot_string == "false":
             download_community_snapshot = False
-        else :
-             download_community_snapshot = True
+        else:
+            download_community_snapshot = True
         return download_community_snapshot
 
     @staticmethod
